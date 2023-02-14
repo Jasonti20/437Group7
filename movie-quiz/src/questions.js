bb4f10f3c731d8bd85aa5e901b2df24838eabfda
huangyuxiang98 <huangyuxiang@wustl.edu>
2023-02-14 17:27:05 -0600

    quiz template

---
+++ movie-quiz/src/questions.js
@@ -0,0 +1,41 @@
+export const questions = [
+  {
+    questionText: "Pick a movie genre you'd most like to watch right now.",
+    answerOptions: [
+      { answerText: "Comedy", isCorrect: true },
+      { answerText: "Drama", isCorrect: true },
+      { answerText: "Thriller", isCorrect: true },
+      { answerText: "Romance", isCorrect: true },
+      { answerText: "Historical", isCorrect: true },
+      { answerText: "Science Fiction", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which movie franchise would you rather marathon on a weekend?",
+    answerOptions: [
+      { answerText: "Star Wars", isCorrect: true },
+      { answerText: "The Avengers", isCorrect: true },
+      { answerText: "Harry Potter", isCorrect: true },
+      { answerText: "Shrek", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which Disney movie made you cry the hardest?",
+    answerOptions: [
+      { answerText: "Up", isCorrect: true },
+      { answerText: "Toy Story 3", isCorrect: true },
+      { answerText: "Inside Out", isCorrect: true},
+      { answerText: "Coco", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which mind-melting movie do you like most?",
+    answerOptions: [
+      { answerText: "Requiem for a Dream", isCorrect: true },
+      { answerText: "Shutter Island", isCorrect: true },
+      { answerText: "Cloud Atlas", isCorrect: true },
+      { answerText: "The Truman Show", isCorrect: true },
+    ],
+  },
+  
+];
