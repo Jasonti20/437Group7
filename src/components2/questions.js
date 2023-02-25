45e48314e4ea72c77a19fc9960581f4aa549f27f
Jasonti20 <112982145+Jasonti20@users.noreply.github.com>
2023-02-25 13:40:34 -0600

    FirstRowResponsive

--- src/components2/questions.js
+++ src/components2/questions.js
@@ -10,32 +10,32 @@ export const questions = [
       { answerText: "Other", isCorrect: true },
     ],
   },
-  {
-    questionText: "Which movie franchise would you rather marathon on a weekend?",
-    answerOptions: [
-      { answerText: "Star Wars", isCorrect: true },
-      { answerText: "The Avengers", isCorrect: true },
-      { answerText: "Harry Potter", isCorrect: true },
-      { answerText: "Shrek", isCorrect: true },
-    ],
-  },
-  {
-    questionText: "Which Disney movie made you cry the hardest?",
-    answerOptions: [
-      { answerText: "Up", isCorrect: true },
-      { answerText: "Toy Story 3", isCorrect: true },
-      { answerText: "Inside Out", isCorrect: true},
-      { answerText: "Coco", isCorrect: true },
-    ],
-  },
-  {
-    questionText: "Which mind-melting movie do you like most?",
-    answerOptions: [
-      { answerText: "Requiem for a Dream", isCorrect: true },
-      { answerText: "Shutter Island", isCorrect: true },
-      { answerText: "Cloud Atlas", isCorrect: true },
-      { answerText: "The Truman Show", isCorrect: true },
-    ],
-  },
+  // {
+  //   questionText: "Which movie franchise would you rather marathon on a weekend?",
+  //   answerOptions: [
+  //     { answerText: "Star Wars", isCorrect: true },
+  //     { answerText: "The Avengers", isCorrect: true },
+  //     { answerText: "Harry Potter", isCorrect: true },
+  //     { answerText: "Shrek", isCorrect: true },
+  //   ],
+  // },
+  // {
+  //   questionText: "Which Disney movie made you cry the hardest?",
+  //   answerOptions: [
+  //     { answerText: "Up", isCorrect: true },
+  //     { answerText: "Toy Story 3", isCorrect: true },
+  //     { answerText: "Inside Out", isCorrect: true},
+  //     { answerText: "Coco", isCorrect: true },
+  //   ],
+  // },
+  // {
+  //   questionText: "Which mind-melting movie do you like most?",
+  //   answerOptions: [
+  //     { answerText: "Requiem for a Dream", isCorrect: true },
+  //     { answerText: "Shutter Island", isCorrect: true },
+  //     { answerText: "Cloud Atlas", isCorrect: true },
+  //     { answerText: "The Truman Show", isCorrect: true },
+  //   ],
+  // },
   
 ];
