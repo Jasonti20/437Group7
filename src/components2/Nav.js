b0708a32d2b4c30fed299d8612370cbac729e0fc
walex42 <walex@wustl.edu>
2023-02-28 14:40:40 -0600

    added clickable nav bar images
    this includes the side bar pop-up now

--- src/components2/Nav.js
+++ src/components2/Nav.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './Nav.css';
+import SideNav from './sidebar';
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const [showNav, setShowNav] = useState(false);
+
+  const handleClick = () => {
+    setShowNav(true);
+  };
+
+  const handleClose = () => {
+    setShowNav(false);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -21,13 +38,16 @@ function Nav() {
         className='nav__logo'
         src='https://github.com/Jasonti20/437Group7/blob/main/logo/logo-image-background.png?raw=true'
         alt='RR Logo'
+        onClick={scrollToTop}
       />
 
       <img
         className='nav__avatar'
         src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
         alt='Avatar'
+        onClick={handleClick}
       />
+      <SideNav showNav={showNav} onClose={handleClose} />
     </div>
   );
 }
